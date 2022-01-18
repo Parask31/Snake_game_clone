e0f5b3f73e614bd947a893b9d50dafefaa1d476c
Paras Kamble <89475543+Parask31@users.noreply.github.com>
2022-01-18 17:46:02 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     # Initializes the score thing at top centre of game using write function of turtle
     def __init__(self):
         super().__init__()
+        with open("data.txt", mode='r') as data:
+            self.high_score = int(data.read())
         self.speed("fastest")
         self.color("white")
         self.hideturtle()
@@ -17,15 +19,18 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.current_score}", False, align= ALIGNMENT, font= FONT)
+        self.clear()
+        self.write(f"Score: {self.current_score} High score:{self.high_score}", False, align= ALIGNMENT, font= FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER.", False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.current_score > self.high_score:
+            self.high_score = self.current_score
+            with open("data.txt", mode='w') as file2:
+                file2.write(str(self.high_score))
+        self.current_score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.current_score += 1
-        self.clear()
         self.update_scoreboard()
-
 
